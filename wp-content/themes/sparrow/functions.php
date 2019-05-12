86d324b9ba825462edabc04a026d1deb68437cca
kurp96@ya.ru <kurp96@ya.ru>
2019-05-12 16:24:41 +0300

    Выведены посты

--- wp-content/themes/sparrow/functions.php
+++ wp-content/themes/sparrow/functions.php
@@ -9,6 +9,36 @@ function myMenu()
 {
   register_nav_menu('top', 'Меню в шапке');
   register_nav_menu('footer', 'Меню в подвале');
+  add_theme_support('title-tag');
+  add_theme_support( 'post-thumbnails', array( 'post' ) );
+  add_image_size('post_thumb', 1300, 500, true);
+  add_filter( 'excerpt_more', 'new_excerpt_more' );
+  function new_excerpt_more( $more ){
+	  global $post;
+	  return '<a href="'. get_permalink($post) . '"> Читать дальше...</a>';
+  }
+  // удаляет H2 из шаблона пагинации
+  add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
+  function my_navigation_template( $template, $class ){
+    /*
+    Вид базового шаблона:
+    <nav class="navigation %1$s" role="navigation">
+      <h2 class="screen-reader-text">%2$s</h2>
+      <div class="nav-links">%3$s</div>
+    </nav>
+    */
+
+    return '
+    <nav class="navigation %1$s" role="navigation">
+      <div class="nav-links">%3$s</div>
+    </nav>    
+    ';
+  }
+
+  // выводим пагинацию
+  the_posts_pagination( array(
+    'end_size' => 2,
+  ) );
 }
 
 function style_theme()
