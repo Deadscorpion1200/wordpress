a32b07f57ac03f5787e18bae197ac66317c7490c
Alexey <kurp96@ya.ru>
2019-05-17 10:46:04 +0300

    Исправлен вывод постов в Single.php

--- wp-content/themes/sparrow/single.php
+++ wp-content/themes/sparrow/single.php
@@ -1,52 +1,52 @@
 <?php get_header(); ?>
 
-   <!-- Page Title
+<!-- Page Title
    ================================================== -->
-   <?php echo get_post_format(); ?>
-   <?php the_post(); ?>
+<?php echo get_post_format(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-   <!-- Content
+<!-- Content
    ================================================== -->
-   <div class="content-outer">
+<div class="content-outer">
 
-      <div id="page-content" class="row">
+   <div id="page-content" class="row">
 
-         <div id="primary" class="eight columns">
+      <div id="primary" class="eight columns">
 
-            <?php get_template_part( 'post-templates/post', get_post_format() ); ?>
+         <?php get_template_part( 'post-templates/post', get_post_format() ); ?>
 
-         </div> <!-- Primary End-->
+      </div> <!-- Primary End-->
 
-         <div id="secondary" class="four columns end">
+      <div id="secondary" class="four columns end">
 
-            <?php get_sidebar(); ?>
+         <?php get_sidebar(); ?>
 
-         </div> <!-- Secondary End-->
+      </div> <!-- Secondary End-->
 
+   </div>
+
+</div> <!-- Content End-->
+
+<!-- Tweets Section
+   ================================================== -->
+<section id="tweets">
+
+   <div class="row">
+
+      <div class="tweeter-icon align-center">
+         <i class="fa fa-twitter"></i>
       </div>
 
-   </div> <!-- Content End-->
-
-   <!-- Tweets Section
-   ================================================== -->
-   <section id="tweets">
-
-      <div class="row">
-
-         <div class="tweeter-icon align-center">
-            <i class="fa fa-twitter"></i>
-         </div>
-
-         <ul id="twitter" class="align-center">
-            <li>
-               <span>
-               This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+      <ul id="twitter" class="align-center">
+         <li>
+            <span>
+               This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
                Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
                <a href="#">http://t.co/CGIrdxIlI3</a>
-               </span>
-               <b><a href="#">2 Days Ago</a></b>
-            </li>
-            <!--
+            </span>
+            <b><a href="#">2 Days Ago</a></b>
+         </li>
+         <!--
             <li>
                <span>
                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
@@ -56,12 +56,17 @@
                <b><a href="#">3 Days Ago</a></b>
             </li>
             -->
-         </ul>
+      </ul>
 
-         <p class="align-center"><a href="#" class="button">Follow us</a></p>
+      <p class="align-center"><a href="#" class="button">Follow us</a></p>
 
-      </div>
+   </div>
 
-   </section> <!-- Tweets Section End-->
+</section> <!-- Tweets Section End-->
+
+<?php endwhile; ?>
+<?php endif; ?>
+
+
 
 <?php get_footer(); ?>
